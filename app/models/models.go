29d51e6389f95c141c05d9b038d19eeffb1d8067
Abdul Ahad <abdul.ahad@plainsurf.com>
2023-07-25 19:59:37 +0530

    cleaned code

--- app/models/models.go
+++ app/models/models.go
@@ -2,18 +2,6 @@
 
 package models
 
-//This is for Mysql
-// Student represents the student entity
-// type Student struct {
-// 	ID               uint     `json:"id" gorm:"primarykey"`
-// 	Name             string   `json:"name"`
-// 	RollNo           string   `json:"rollno"`
-// 	Class            string   `json:"class"`
-// 	OptionalSubjects []string `json:"optional_subjects" gorm:"type:json"`
-// 	StudentRank      int      `json:"student_rank"` // Renamed from "Rank" to "StudentRank"
-// }
-
-// This is working for mongoDB
 type Student struct {
 	ID               uint     `json:"id" gorm:"primarykey"`
 	Email            string   `json:"email"`
@@ -25,12 +13,4 @@ type Student struct {
 	StudentRank      int      `json:"student_rank"`
 }
 
-// This is working for postgress sql
-// type Student struct {
-// 	ID               string   `json:"id" gorm:"primarykey"`
-// 	Name             string   `json:"name"`
-// 	RollNo           string   `json:"rollno"`
-// 	Class            string   `json:"class"`
-// 	OptionalSubjects []string `json:"optional_subjects" gorm:"type:text[]"` // I'm getting error here
-// 	Rank             int      `json:"student_rank"`
-// }
+
